69c8b581453e1e3999a43bce5b6ffb8f353a00e0
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-11-08 12:31:14 -0500

    Fetched to rails api and random img api, and rendered to the petlist

--- src/components/PetItem.js
+++ src/components/PetItem.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 
 class PetItem extends Component {
- state = {}
+ state = {
+   imgUrl: ''
+ }
+
+ componentDidMount(){
+   fetch('https://dog.ceo/api/breeds/image/random')
+   .then(res => res.json())
+   .then(imgObject => {
+    //  console.log(imgObject)
+     this.setState({
+       imgUrl: imgObject.message
+     })
+   })
+ }
  render() {
+   const {name, age, breed, location} = this.props.pet
+  //  console.log(this.props.pet)
   return(
-   <div>
-       Pet Item 
+   <div> 
+ 
+     <img className="dog-img" src={this.state.imgUrl} alt={name}/>
+     <h2>{name} </h2>
    </div>
     )
    }
--- src/containers/PetContainer.js
+++ src/containers/PetContainer.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react'
 import PetList from '../components/PetList'
 
 class PetContainer extends Component {
- state = {}
+ state = {
+   pets: []
+ }
+ 
+ componentDidMount(){
+   fetch('http://localhost:3000/pets')
+   .then(res => res.json())
+   .then(petsData => {
+    //  console.log(petsData)
+    this.setState({
+      pets: petsData
+    })
+   })
+ }
+  
+ 
+
  render() {
   return(
    <div className="pet-container">
        Pet Container
-       <PetList/>
+       <PetList pets={this.state.pets}/>
    </div>
     )
    }
