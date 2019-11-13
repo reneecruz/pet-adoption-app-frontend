4eb60af3c94462bf104b4bfa7f234390bc36cd81
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-11-12 20:53:39 -0500

    refactored

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -9,8 +9,8 @@ class PetCard extends Component {
      "Pet card ~~~~~ ", this.props.pet)
   return(
    <div className="dog-card-container"> 
-      <div className="dog-card" onClick={() => this.props.handleClick(this.props.pet)}>
-          <img className="dog-card-img" src={img} alt={name}/>
+      <div className="dog-card" onClick={() => this.props.handlePetGoBack(this.props.pet)}>
+          <img className="dog-card-img" src={img} alt={name} title={name}/>
           <h2><b>Name : </b>{name} </h2>
           <p><b>Age : </b>{age} </p>
           <p><b>Breed : </b> {breed}</p>
@@ -19,7 +19,11 @@ class PetCard extends Component {
       <div className="clear"></div>
      </div> 
      <div>
-      {this.props.pet.adoption ? `Im Adopted!! by ${this.props.pet.adoption.user.name}` : <button onClick={() => this.props.adoptDoggy(this.props.pet)}>Adopt Me ❣️ </button>}
+      {this.props.pet.adoption ? 
+        `Im Adopted!! by ${this.props.pet.adoption.user.name}` : 
+        <button onClick={() => this.props.adoptDoggy(this.props.pet)}>
+          Adopt Me ❣️
+        </button>}
     </div> 
    </div>
     )
--- src/components/PetItem.js
+++ src/components/PetItem.js
@@ -8,15 +8,16 @@ class PetItem extends Component {
   //  console.log(this.props.pet)
   return(
    <div className="dog-item-container"> 
-     <div className="dog-item" onClick={() => this.props.handleClick(this.props.pet)}>
+     <div className="dog-item" onClick={() => this.props.handlePetView(this.props.pet)}>
         <div className="dog-img-div">
          <img className="dog-img" src={img} alt={name}/>
         </div>
      <h2>{name} </h2>
+     <p> { this.props.pet.adoption ? 'I am adopted' : "Adopt me"}</p>
      <div className="clear"></div>
    </div>
        
-       
+            
      
    </div>
     )
