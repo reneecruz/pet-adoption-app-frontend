046d141b3180619dcc42bc782aa9e642c31ce3a8
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-11-12 23:16:58 -0500

    refactored

--- src/App.js
+++ src/App.js
@@ -7,35 +7,16 @@ import NavBar from './components/NavBar';
 
 function App() {
 
-  // const renderButtons = () => {
-  //   if (localStorage.token) {
-  //    return <NavLink className="nav-link" to='/'>Home</NavLink>   
-  //   }else {
-  //     return (
-  //       <>
-  //       <NavLink className="nav-link" to='/'>Home</NavLink>
-  //       <NavLink className="nav-link" to='/signup'>Sign Up</NavLink>
-  //       </>
-  //     )
-  //   }
-  // }
 
   return (
     
     
     <div className="App">
-{/* 
-  //     <header className="header"> 
-  //       <h1>
-  //         DoggoDoption
-  //       </h1>
-  //       {this.renderButtons()}    
-  //     </header> */}
+
     <NavBar />
     <Switch>
 
       <Route exact path={'/'} render={renderProps => <HomeContainer renderProps={renderProps}/>} />
-      {/* <Route exact path ={"/prpfile"} render={renderProps => <HomeContainer renderProps={renderProps}/>}/> */}
       <Route exact path ={"/signup"} render={renderProps => <HomeContainer renderProps={renderProps}/>}/>
       
     </Switch>
